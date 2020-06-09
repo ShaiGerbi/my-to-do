ce2d38b0a632249988e00ce9bc88abda95478d17
Shai Gerbi <ShaiGerbi@gmail.com>
2020-06-09 21:58:18 +0300

    Install vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
 </template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
